9cecf38476b46745a66da349e830007071931b5f
Rafael <raf_berm2@yahoo.com>
2019-11-05 01:21:30 -0500

    Added static folder

--- ossac/settings.py
+++ ossac/settings.py
@@ -114,9 +114,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
+

0a670d2e0063d1915ee6eda9d07c0244da26573b
Rafael <raf_berm2@yahoo.com>
2019-11-05 01:12:23 -0500

    Adeed config for static filesç

--- ossac/settings.py
+++ ossac/settings.py
@@ -114,8 +114,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)

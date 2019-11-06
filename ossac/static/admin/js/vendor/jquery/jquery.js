9d61f1a22e45592aa21c76a729d7299ade1a4012
Rafael <raf_berm2@yahoo.com>
2019-11-05 22:48:12 -0500

    Test for static files

--- ossac/static/admin/js/vendor/jquery/jquery.js
+++ ossac/static/admin/js/vendor/jquery/jquery.js
@@ -9948,8 +9948,8 @@ jQuery.offset = {
 			curElem = jQuery( elem ),
 			props = {};
 
-		// Set position first, in-case top/left are set even on static elem
-		if ( position === "static" ) {
+		// Set position first, in-case top/left are set even on staticfiles elem
+		if ( position === "staticfiles" ) {
 			elem.style.position = "relative";
 		}
 
@@ -10057,7 +10057,7 @@ jQuery.fn.extend( {
 			offsetParent = elem.offsetParent || doc.documentElement;
 			while ( offsetParent &&
 				( offsetParent === doc.body || offsetParent === doc.documentElement ) &&
-				jQuery.css( offsetParent, "position" ) === "static" ) {
+				jQuery.css( offsetParent, "position" ) === "staticfiles" ) {
 
 				offsetParent = offsetParent.parentNode;
 			}
@@ -10091,7 +10091,7 @@ jQuery.fn.extend( {
 		return this.map( function() {
 			var offsetParent = this.offsetParent;
 
-			while ( offsetParent && jQuery.css( offsetParent, "position" ) === "static" ) {
+			while ( offsetParent && jQuery.css( offsetParent, "position" ) === "staticfiles" ) {
 				offsetParent = offsetParent.offsetParent;
 			}
 

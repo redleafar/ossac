790ebdb09ad4a77dd297cbf51b9734a33d6d5eb2
Rafael <raf_berm2@yahoo.com>
2020-01-10 11:48:39 -0500

    Added choices for status in admin

--- ossacmain/models.py
+++ ossacmain/models.py
@@ -3,5 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Installation(models.Model):
-    name = models.CharField(max_length=200)
-    type = models.CharField(max_length=100)
+    STATUS = (
+        ('good', ('En buen estado')),
+        ('medium', ('Algo está ocurriendo')),
+        ('bad', ('Defectuoso'))
+    )
+
+    name = models.CharField(max_length = 200)
+    type = models.CharField(max_length = 100)
+    detail = models.CharField(max_length = 500)
+    status = models.CharField(max_length = 100, choices = STATUS,  default = "good")
